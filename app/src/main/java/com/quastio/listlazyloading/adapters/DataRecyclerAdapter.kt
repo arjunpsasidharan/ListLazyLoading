71ce7527d17e46b273263ed0b294c567d690d8ef
arjunp <>
2020-08-22 15:56:33 +0530

    added ui in adapter list

--- app/src/main/java/com/quastio/listlazyloading/adapters/DataRecyclerAdapter.kt
+++ app/src/main/java/com/quastio/listlazyloading/adapters/DataRecyclerAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
@@ -12,6 +13,7 @@ import com.bumptech.glide.Glide
 import com.quastio.listlazyloading.R
 import com.quastio.listlazyloading.model.ResponseModel
 import com.quastio.listlazyloading.utils.DataDiffUtil
+import com.quastio.listlazyloading.utils.DateAndTimeUtils
 
 class DataRecyclerAdapter(private val interaction: Interaction? = null) :
     PagingDataAdapter<ResponseModel,RecyclerView.ViewHolder>(DataDiffUtil.DIFF_CALLBACK) {
@@ -46,6 +48,10 @@ class DataRecyclerAdapter(private val interaction: Interaction? = null) :
         private val interaction: Interaction?
     ) : RecyclerView.ViewHolder(itemView) {
         private val imageView:ImageView=itemView.findViewById(R.id.image_view)
+        private val statusImageView:ImageView=itemView.findViewById(R.id.status_iv)
+        private val dateTextView:TextView=itemView.findViewById(R.id.date_tv)
+        private val timeTv:TextView=itemView.findViewById(R.id.time_tv);
+        private val sizeTv:TextView=itemView.findViewById(R.id.size_tv);
 
         fun bind(item: ResponseModel) = with(itemView) {
             itemView.setOnClickListener {
@@ -55,12 +61,40 @@ class DataRecyclerAdapter(private val interaction: Interaction? = null) :
                 .load(item.thumbnail)
                 .into(imageView)
 
+            item.dateTime?.let {
+                val date=DateAndTimeUtils.getDate(it)
+                dateTextView.text=date
+                val time=DateAndTimeUtils.getTime(it)
+                timeTv.text="@ $time"
+            }
+            item.status?.let {
+                when(it){
+                    STATUS_DOWNLOADED->{
+                    statusImageView.setImageResource(R.drawable.ic_status_downloaded)
+                    }
+                    STATUS_UPLOADED->{
+                        statusImageView.setImageResource(R.drawable.ic_status_uploaded)
+
+                    }
+                    else->{
+                        statusImageView.setImageResource(R.drawable.ic_status_none)
+                    }
+                }
+            }
+
+            item.fileSize?.let {
+                sizeTv.text=it
+            }
         }
     }
 
     interface Interaction {
         fun onItemSelected(position: Int, item: ResponseModel)
     }
-
+    companion object{
+        private const val STATUS_NONE="STATUS_NONE"
+        private const val STATUS_DOWNLOADED="STATUS_DOWNLOADED"
+        private const val STATUS_UPLOADED="STATUS_UPLOADED"
+    }
 }
 
---
+++ app/src/main/java/com/quastio/listlazyloading/utils/DateAndTimeUtils.kt
@@ -0,0 +1,36 @@
+package com.quastio.listlazyloading.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DateAndTimeUtils {
+    fun getDate(date: String): String? {
+        var date = date
+        return try {
+            date = date.replace("T", " ").replace("Z", "")
+            val sdf =
+                SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val dateUtc = sdf.parse(date)
+            val sdfLocal =
+                SimpleDateFormat("yyyy/MM/dd", Locale.getDefault())
+                sdfLocal.format(dateUtc)
+        } catch (e: Exception) {
+            ""
+        }
+    }
+    fun getTime(date: String): String? {
+        var date = date
+        return try {
+            date = date.replace("T", " ").replace("Z", "")
+            val sdf =
+                SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+            val dateUtc = sdf.parse(date)
+            val sdfLocal =
+                SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+            sdfLocal.format(dateUtc)
+        } catch (e: Exception) {
+            ""
+        }
+    }
+
+}
